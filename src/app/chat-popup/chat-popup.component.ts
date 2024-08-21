1398295cdefa288c58a324aedbc169c5273f3a4d
Slashy Fx <amirramy.chatbi@gmail.com>
2024-08-21 02:58:25 +0200

    fix

--- src/app/chat-popup/chat-popup.component.ts
+++ src/app/chat-popup/chat-popup.component.ts
@@ -1,21 +1,27 @@
 import { Component } from '@angular/core';
-import { NgIf } from '@angular/common';  // Importer NgIf directement
 
 @Component({
   selector: 'app-chat-popup',
   templateUrl: './chat-popup.component.html',
   styleUrls: ['./chat-popup.component.scss'],
-  standalone: true,
-  imports: [NgIf]  // Importer NgIf directement
+  standalone: true
 })
 export class ChatPopupComponent {
   isOpen = false;
 
   openPopup() {
     this.isOpen = true;
+    const popup = document.getElementById('popup');
+    if (popup) {
+      popup.classList.add('active');
+    }
   }
 
   closePopup() {
     this.isOpen = false;
+    const popup = document.getElementById('popup');
+    if (popup) {
+      popup.classList.remove('active');
+    }
   }
 }
